42b94be6ec3d0289e956fbaf1b4b575a9abe03dc
Serg Bugayov <serg.bugayov@gmail.com>
2026-06-08 20:42:43 +0000

    add string-helper

---
+++ src/Helper/StringHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Corvet\LightFieldBundle\Helper;
+
+class StringHelper
+{
+    public static function TrimOrNull(?string $value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+        $value = trim($value);
+
+        return !empty($value) ? $value : null;
+    }
+
+    public static function NormalizeEmail(?string $value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+        $value = trim($value);
+
+        return !empty($value) ? mb_strtolower($value) : null;
+    }
+}
