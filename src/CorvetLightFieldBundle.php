3d9c4b6c258890388a1cc4db949e735888584ccb
Serg Bugayov <serg.bugayov@gmail.com>
2026-06-25 22:29:34 +0300

    2026-06-25 22:29:34 +0300 - auto-commit

--- src/CorvetLightFieldBundle.php
+++ src/CorvetLightFieldBundle.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Corvet\LightFieldBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
@@ -28,6 +27,7 @@ class CorvetLightFieldBundle extends AbstractBundle
                 ],
             ]);
         }
+
         // 2. Налаштування AssetMapper (Замість старого Extension-класу)
         if ($builder->hasExtension('framework')) {
             $container->extension('framework', [
@@ -38,41 +38,18 @@ class CorvetLightFieldBundle extends AbstractBundle
                     ],
                 ],
             ]);
-            // ----- 1.
-            // Додатково реєструємо Stimulus-директорію, якщо увімкнено StimulusBundle
-            // if ($builder->hasExtension('stimulus')) {
-            //     $container->extension('stimulus', [
-            //         'controller_paths' => [
-            //             // Кажемо Symfony, що в цій папці лежать контролери бандлу
-            //             dirname(__DIR__) . '/assets/controllers',
-            //         ],
-            //     ]);
-            // }
-            // ----- 2
-            // if ($builder->hasExtension('stimulus')) {
-            //     $builder->prependExtensionConfig('stimulus', [
-            //         // Symfony Flex та StimulusBundle налаштовані так, що при повторному
-            //         // виклику prependExtensionConfig для секції `controller_paths`
-            //         // масиви будуть автоматично ЗОБ'ЄДНАНІ, а не замінені.
-            //         'controller_paths' => [
-            //             dirname(__DIR__) . '/assets/controllers',
-            //         ],
-            //     ]);
-            // }
         }
     }
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        // Налаштовуємо автоконфігурацію для класів бандла
         $services = $container->services();
 
         $services->defaults()
             ->autowire()
-            ->autoconfigure(); // Це найважливіше: воно змусить Symfony бачити атрибут #[AsCommand]
+            ->autoconfigure();
 
-        // Автоматично завантажуємо команду з папки Command
         $services->set(Command\CorvetInstallCommand::class)
-            ->tag('console.command'); // Явно вказуємо тег для консольних команд
+            ->tag('console.command');
     }
 }
